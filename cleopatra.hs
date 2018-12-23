817e8636c982bd9ff3bf0ada0218cd5ea21f10cf
Yvan Sraka <yvan@sraka.pw>
2018-12-23 23:09:34 +0100

    v0.4.0

--- cleopatra.hs
+++ cleopatra.hs
@@ -1,19 +1,34 @@
 #!/usr/bin/env stack
 -- stack --install-ghc runghc
 
+import Data.Maybe
 import System.Environment
 import System.Process
 
 main :: IO ()
 main = do
-    -- TODO Pattern match failure if no args were given
-    command:args <- getArgs
-    command # args
+    args <- getArgs
+    run args
 
-(#) :: String -> [String] -> IO ()
+run :: [String] -> IO ()
+run (cmd:args) = fromMaybe (\x -> err (cmd:x)) (lookup cmd dispatch) args
+run _ = help []
 
-"--help" # _ = putStrLn "\
-    \Cleopatra 0.3.0\n\
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [ ("add", add)
+           , ("remove", remove)
+           , ("glue", glue)
+           , ("unglue", unglue)
+           , ("-h", help)
+           , ("--help", help)
+           , ("-V", version)
+           , ("--version", version)
+           ]
+
+--help, -h
+help :: [String] -> IO ()
+help _ = putStrLn "\
+    \Cleopatra 0.4.0\n\
     \Yvan SRAKA <yvan@sraka.pw>\n\
     \Micro virtual environment for YeAST\n\
     \\n\
@@ -32,36 +47,42 @@ main = do
                         \ where ./cleopatra folder is in PATH\n\
     \    unglue           Exit the virtual environment without leaving current\
                         \ opened shell"
-"-h" # _ = "--help" # []
 
-"--version" # _ = putStrLn "Cleopatra 0.1.0"
-"-V" # _ = "--version" # []
+--version, -V
+version :: [String] -> IO ()
+version _ = putStrLn "Cleopatra 0.1.0"
 
-"add" # (x:xs) = do
+err :: [String] -> IO ()
+err (cmd:args) = putStrLn ("\x1b[31merror:\x1b[0m\
+    \Found command '" ++ cmd ++ "' which wasn't expected, or isn't valid in \
+    \this context")
+err [] = return ()
+
+add :: [String] -> IO ()
+add (x:xs) = do
     _ <- system ("mkdir -p .cleopatra \
              \ && echo \"YEAST_CONTEXT=$(which " ++ x ++ ") yeast \\$@\" \
                  \ > .cleopatra/" ++ x ++ " \
              \ && chmod +x .cleopatra/" ++ x)
     -- TODO putStrLn
-    "add" # xs
-"add" # [] = return ()
+    add xs
+add [] = return ()
 
-"remove" # (x:xs) = do
+remove :: [String] -> IO ()
+remove (x:xs) = do
     _ <- system ("rm .cleopatra/" ++ x)
     -- TODO putStrLn
-    "remove" # xs
-"remove" # [] = return ()
+    remove xs
+remove [] = return ()
 
-"glue" # _ = do
+glue :: [String] -> IO ()
+glue _ = do
     -- TODO putStrLn
     _ <- system "PATH=.cleopatra:$PATH $SHELL"
     return ()
 
-"unglue" # _ = do
+unglue :: [String] -> IO ()
+unglue _ = do
     -- TODO Ctrl+D instruction
     _ <- system "export PATH=${PATH%:.cleopatra/}"
     return ()
-
-cmd # _ = putStrLn ("\x1b[31merror:\x1b[0m \
-    \Found argument '" ++ cmd ++ "' which wasn't expected, or isn't valid in \
-    \this context")
